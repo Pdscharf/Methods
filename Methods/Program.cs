5d6c2b5a5588d82c1f80bae6db6d107e1ca44812
Paul Scharf <pdscharf@usf.edu>
2021-02-05 22:57:49 -0500

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello" + " " + name + "!");
+            Console.WriteLine("Press enter to exit the program...");
+            Console.ReadKey(true);
+            Console.ReadLine();
+        }
+    }
+}
